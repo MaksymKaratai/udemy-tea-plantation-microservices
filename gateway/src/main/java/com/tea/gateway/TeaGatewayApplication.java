169776e22226c1a6d4a071b3ca0ed6df04670fb8
MaksymKaratai <mximkarat@gmail.com>
2023-04-17 17:29:41 +0300

    Gateway service

---
+++ gateway/src/main/java/com/tea/gateway/TeaGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.tea.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TeaGatewayApplication.class, args);
+    }
+}
