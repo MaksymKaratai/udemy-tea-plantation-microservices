06df15607d66abe6e079436e194636b144de9269
MaksymKaratai <mximkarat@gmail.com>
2023-04-29 22:57:07 +0300

    Add distributed tracing(Zipkin) support for services

--- tea-order/src/main/java/com/tea/order/configuration/FeignConfig.java
+++ tea-order/src/main/java/com/tea/order/configuration/FeignConfig.java
@@ -1,11 +1,20 @@
 package com.tea.order.configuration;
 
 import com.tea.order.services.client.PlantationClient;
+import feign.Capability;
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Profile("!local")
 @Configuration
 @EnableFeignClients(clients = {PlantationClient.class})
-public class FeignConfig {}
+public class FeignConfig {
+    @Bean
+    public Capability micrometerCapability(final MeterRegistry registry) {
+        return new MicrometerCapability(registry);
+    }
+}
--- tea-plantation/src/main/java/com/tea/plantation/configuration/FeignConfig.java
+++ tea-plantation/src/main/java/com/tea/plantation/configuration/FeignConfig.java
@@ -1,7 +1,11 @@
 package com.tea.plantation.configuration;
 
 import com.tea.plantation.services.client.InventoryClient;
+import feign.Capability;
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -9,4 +13,8 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @EnableFeignClients(clients = {InventoryClient.class})
 public class FeignConfig {
+    @Bean
+    public Capability micrometerCapability(final MeterRegistry registry) {
+        return new MicrometerCapability(registry);
+    }
 }
